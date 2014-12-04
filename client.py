ebd39e7322bacdb88c2376ed94421ca016fefe12
alvaromv83 <alvaromv83@gmail.com>
2014-12-04 20:35:21 +0100

    Añadidas mejoras en client.py

--- client.py
+++ client.py
@@ -18,10 +18,10 @@ try:
     parameters = sys.argv[2]
 
     ADDRESS = parameters.split(':')[0]
-    IP = ADDRESS.split('@')[1]
-    PORT = int(parameters.split(':')[1])
+    SERV_IP = ADDRESS.split('@')[1]
+    SERV_PORT = int(parameters.split(':')[1])
 except:
-    print ("Usage: python client.py method receiver@IP:SIPport")
+    print ("Usage: python client.py method receiver@ServerIP:SIPport")
     raise SystemExit
 
 # Contenido que vamos a enviar
@@ -30,7 +30,7 @@ request = method + " " + PROTOCOL + ":" + ADDRESS + " " + VERSION + "\r\n\r\n"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((IP, PORT))
+my_socket.connect((SERV_IP, SERV_PORT))
 
 # Enviamos solicitud y recibimos respuesta
 my_socket.send(request)
@@ -38,12 +38,13 @@ print "Enviado:\n" + request
 try:
     data = my_socket.recv(1024)
 except socket.error:
-    print ("Error: No server listening at " + IP + " port " + str(PORT))
+    print ("Error: No server listening at " + SERV_IP + " port "\
+           + str(SERV_PORT))
     raise SystemExit
 
 print 'Recibido:\n' + data
 
-# Si recibimos confirmación de INVITE envíamos ACK
+# Si recibimos confirmación de INVITE envíamos ACK y recibimos contenido
 response1 = "SIP/1.0 100 Trying\r\n\r\n" + "SIP/1.0 180 Ringing\r\n\r\n"\
           + "SIP/1.0 200 OK\r\n\r\n"
 response2 = "SIP/2.0 100 Trying\r\n\r\n" + "SIP/2.0 180 Ringing\r\n\r\n"\
@@ -54,6 +55,9 @@ if data == response1 or data == response2:
               + "\r\n\r\n"
     my_socket.send(request)
     print "Enviado:\n" + request
+    print "Recibiendo contenido..."
+    data = my_socket.recv(1024)
+    print "Recepción finalizada"
 
 print "Finalizando socket..."
 
